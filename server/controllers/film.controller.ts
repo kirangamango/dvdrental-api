6788e04cc87afe2c6d35e844c6db6bbb7478d626
Kiran Kumar <kirangamango@gmail.com>
2024-04-02 15:21:05 +0530

    join, pagination

--- server/controllers/film.controller.ts
+++ server/controllers/film.controller.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { prisma } from "../configs";
 import { RequestHandler } from "express";
 
@@ -6,12 +7,29 @@ export const FilmController: {
 } = {
   async getAll(req, res, next) {
     try {
-      const films =
-        await prisma.$queryRaw`select film_id, title, description, release_year, language_id, rental_duration, rental_rate, rating, length, replacement_cost, special_features, last_update from film`;
+      let { page = "0", limit = "20", isActorNull } = req?.query;
+
+      const pageNo = Number(page);
+      const skip = pageNo ? (pageNo - 1) * Number(limit) : Number(limit);
+
+      console.log({ page, limit, pageNo, skip });
+
+      let paginationStr = `offset ${skip} limit ${Number(limit)}`;
+
+      const queryStr =
+        isActorNull === "true" ? "where actor_id is null" : undefined;
+
+      const baseQuery = `select * from (select f1.film_id,title,fa1.actor_id,first_name, last_name  from film f1 
+        right join film_actor fa1 on f1.film_id = fa1.film_id 
+        left join actor a1 on a1.actor_id = fa1.actor_id) ${queryStr} ${paginationStr}`;
+
+      console.log({ isActorNull, queryStr });
+      const films: any = await prisma.$queryRaw`${Prisma.raw(baseQuery)}`;
 
       res.json({
         success: true,
         data: films,
+        total: films.length,
       });
     } catch (err) {
       console.error({ err });
